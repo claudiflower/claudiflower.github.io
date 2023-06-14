e123ce887f76e16bb4c9662059b0386f64e9ceec
claudiflower <liharcl@gmail.com>
2023-06-14 16:56:49 -0400

    Completely redesigned website.

--- pScript.js
+++ pScript.js
@@ -17,5 +17,26 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    if (slideIndex-1===0) {
+        slides[slideIndex-1].style.display = "block";
+        document.getElementById("info").style = "color:#dfa1a6";
+        document.getElementById("abt").style = "color:#c28489";
+    } else if (slideIndex-1===1) {
+        slides[slideIndex-1].style.display = "block";
+        document.getElementById("info").style = "color:#777345";
+        document.getElementById("abt").style = "color:#e91d28";
+    } else if (slideIndex-1===2) {
+        slides[slideIndex-1].style.display = "block";
+        document.getElementById("info").style = "color:#8fabb9";
+        document.getElementById("abt").style = "color:#6e8d99";
+    } else if (slideIndex-1===3) {
+        slides[slideIndex-1].style.display = "block";
+        document.getElementById("info").style = "color:#8fabb9";
+        document.getElementById("abt").style = "color:#6e8d99";
+    } else if (slideIndex-1===4) {
+        slides[slideIndex-1].style.display = "block";
+        document.getElementById("info").style = "color:#9eab9e";
+        document.getElementById("abt").style = "color:#374646";
+    }
     slides[slideIndex-1].style.display = "block";
 }
